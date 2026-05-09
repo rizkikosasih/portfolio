73184e54cc299afe21903604279f9e7fb845fe5a
Rizki Kosasih <rizkikosasih1997@gmail.com>
2026-05-09 13:56:22 +0700

    feat: add ThemeProvider component for theme management

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,8 @@
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps,
+} from 'next-themes'
+
+export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
