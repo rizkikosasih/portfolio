cd9c25f599559f062e732d91fa9ae643b4c3cf92
Rizki Kosasih <rizkikosasih1997@gmail.com>
2026-05-14 00:50:25 +0700

    fix: handle optional className in SectionHeader component

--- src/components/layout/SectionHeader.tsx
+++ src/components/layout/SectionHeader.tsx
@@ -1,14 +1,14 @@
 const SectionHeader = ({
   title,
   subtitle,
-  className
+  className,
 }: {
   title: string
-  subtitle: string,
+  subtitle: string
   className?: string
 }) => {
   return (
-    <div className={`mb-12 flex flex-col gap-2 ${className}`}>
+    <div className={`mb-12 flex flex-col gap-2 ${className ?? ''}`}>
       <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">
         {title}
       </h2>
