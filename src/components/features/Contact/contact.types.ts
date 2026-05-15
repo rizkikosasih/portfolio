c0242e5e742d435a86775bc4987911bd28fcea3e
Rizki Kosasih <rizkikosasih1997@gmail.com>
2026-05-15 14:51:55 +0700

    fix(naming): resolve duplicate folder casing issue in remote

--- src/components/features/Contact/contact.types.ts
+++
@@ -1,3 +0,0 @@
-import type { Contact } from '@/types/sanity.types'
-
-export type ContactData = Contact
--- src/components/features/Education/education.types.ts
+++
@@ -1,9 +0,0 @@
-import type { Education } from '@/types/sanity.types'
-import type { Variants } from 'framer-motion'
-
-export type EducationData = Education
-
-export type EducationCardProps = {
-  edu: EducationData
-  variants: Variants
-}
