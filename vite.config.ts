182c02b1cc5413eab85cd53796f2d77cc94f7865
Rizki Kosasih <rizkikosasih1997@gmail.com>
2026-05-15 14:37:26 +0700

    feat(config): add vite-plugin-svgr to support svg as react components

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
