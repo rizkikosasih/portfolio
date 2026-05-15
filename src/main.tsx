54c334badc10f1160521f85add83e33f97e28616
Rizki Kosasih <rizkikosasih1997@gmail.com>
2026-05-15 18:58:12 +0700

    feat(router): replace App component with router setup for improved navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import '@/styles/index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+// Import Layout & Pages
+import { HomeLayout } from '@/components/layout'
+import NotFound from '@/components/NotFound'
 import { ThemeProvider } from '@/components/theme-provider'
+
+import '@/styles/index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>,
 )
